7ac67f71e97ad33c4f08616881ccdbf1bca933e9
alinaugustin <alindebian@gmail.com>
2019-05-05 00:48:12 +0300

    Update argsopts.route.js

--- api/routes/argsopts.route.js
+++ api/routes/argsopts.route.js
@@ -30,50 +30,54 @@ argsRoutes.route('/apiargs/argsopts', verifyToken).get(function(req, res) {
 
   argsRoutes.route('/apiargs/insertargsopts', verifyToken).post(function(req, res) {
     if(verifyToken) {
-    console.log(req.body.optiuni)
-    console.log(req.body.args)
-    console.log(req.body.token)
+    //console.log(req.body.optiuni) console.log(req.body.args) console.log(req.body.token)
     let token2 = req.body.token
     jwt.verify(token2, config.secret, function(err, decoded) {
       if (err)
         return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' })
       // if everything is good, save to request for use in other routes
       let idd = decoded.id
-    
-    Users.findById({_id: idd}, function (err, responses){
-      if(err){
+      Users.findById({_id: idd}, function (err, responses){
+        if(err){
         console.log(err)
-      }
-      console.log('responses: ',responses)
+        }
+        //console.log('responses: ',responses)
         //res.json(responses)
-      if(responses) 
-          {
-            Argsopts.create({
-              optiuni : req.body.optiuni,
-              argum : req.body.args,
-              email : responses.email
-            },
-            function (err, user) {
-              if (err) {
-                console.log('err4:',err)
-                return res.status(500).send("There was a problem registering the user`.")
-              }  
-            })  
-            
-            
-          } else return console.log('Este o eroare cu lungimea!')
-        
-      })
-
-      })
-    
-  
-  
-  } else {
-      console.error('Authorization required!')
-    }
-  
+      if(Object.keys(responses).length > 0) {
+            Argsopts.findOne({email: responses.email}, function (err, respons){
+              if(err) {
+                //console.log(err)
+                  Argsopts.create({
+                  optiuni : req.body.optiuni,
+                  argum : req.body.args,
+                  email : responses.email
+                  },
+                  function (err, respons) {
+                    if (err) {
+                      console.log('err create:',err)
+                    }
+                    console.log('respons: ', respons)
+                    })
+                } else {
+                  //update it
+                  Argsopts.findOneAndUpdate({email: responses.email}, 
+                    { $set: {optiuni: req.body.optiuni, argum: req.body.args} }, 
+                    {useFindAndModify: true}, function(err, rsp) {
+                    if (err) {
+                      console.log('err update:',err)
+                      return (console.log('Nu am putut sa il updatam! + err: '+ err))
+                    }
+                    console.log('rsp: ', rsp)
+                  })
+                }
+              })
+        } else return console.log('responses: ',responses)
+    })
   })
+}
+})
+
+        
 
 
 
