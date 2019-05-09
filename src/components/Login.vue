bf91641a00e8df2dab1d1d9add0c160546b8e534
alinaugustin <alindebian@gmail.com>
2019-05-09 10:58:20 +0300

    modify css

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,36 +1,40 @@
 <template>
-       <div class="col-sm-6">
-         <div v-if="errors && errors.length">
-        <div v-for="error of errors" v-bind:key='error.id'>
-          <div show>{{error.message}}</div>
-        </div>
-      </div>
+
+<div class="container">
+<div class="row">
+  <div class="col-md-9 col-md-offset-3">
+    <div class="well well-sm">
 
     <form class="login" @submit.prevent="login">
     
-    <div class="col-sm-6">
+    <div class="form-group">
     <label>User:</label>
-    <input type="email" id="email" autocomplete="email-ul-inregistrat" placeholder="email" v-model="email" required >
+    <input type="email" id="email" autocomplete="email-ul-inregistrat" placeholder="email" v-model="email" required >  
     </div>
-    <div class="col-sm-6">
-    <span>utilizator: {{ email }}</span>
-    </div>
+    <span>utilizator: {{ email }}</span><br>
 
-    <div class="col-sm-6">
+    <div class="form-group">
     <label>Pass:</label>
     <input type="password" id="parola " autocomplete="parola-curenta" placeholder="parola-curenta" v-model="password" minlength="4" required>
-    </div>
-    <div class="col-sm-6">
+    <br>
     <span>parola: {{ password }}</span><br><br>
     </div>
 
-    <div class="col-sm-6">
     <button id="button" class="btn btn-primary" >Trimite</button>
-    </div>
+    <br>
 
     </form>
 
+        <div v-if="errors && errors.length">
+        <div v-for="error of errors" v-bind:key='error.id'>
+          <div show>{{error}}</div>
+        </div>
+      </div>
+
        </div>
+  </div>
+</div>
+</div>
       </template>
       <script>
       export default {
@@ -46,10 +50,27 @@
             let email = this.email;
             let password = this.password;
             this.$store
+              //.dispatch('auth_error', errors)
               .dispatch("login", { email, password })
               .then(() => this.$router.push("/"))
-              .catch(errors => { alert(" Utilizatorul nu exista sau parola nu e corecta! Eroare: " + this.$store.errors)})
-          }
+              
+                        }
         }
       }
       </script>
+
+
+  <style scoped>
+  .form-group, .container {
+  font-size: 16px;
+  padding: 10px;
+  text-align: center;
+  background-color: rgb(134, 152, 204);
+  color: rgb(0, 2, 8);
+  border: solid 1px #a6d8a8;
+  margin: auto;
+  height: 50%;
+  width: 50%;
+}
+      </style>
+      
